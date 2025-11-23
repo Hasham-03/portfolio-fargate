bc531dc61fc80609af520d07d0acf39890ce90c5
Hasham-03 <hashim332004@gmail.com>
2025-11-23 21:41:16 +0530

    First deployment

---
+++ main.tf
@@ -0,0 +1,103 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+ 
+  backend "s3" {
+    bucket = "hasham-terraform-state-123" 
+    key    = "portfolio/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# 1. ECR Repo 
+resource "aws_ecr_repository" "repo" {
+  name         = "portfolio-repo"
+  force_delete = true
+}
+
+# 2. Networking 
+data "aws_vpc" "default" { default = true }
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# 3. Security Group
+resource "aws_security_group" "lb_sg" {
+  name        = "portfolio-sg"
+  vpc_id      = data.aws_vpc.default.id
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 4. IAM Role 
+resource "aws_iam_role" "execution_role" {
+  name = "portfolio_execution_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "ecs-tasks.amazonaws.com" } }]
+  })
+}
+resource "aws_iam_role_policy_attachment" "ecs_policy" {
+  role       = aws_iam_role.execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# 5. ECS Cluster & Task
+resource "aws_ecs_cluster" "cluster" { name = "portfolio-cluster" }
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = "portfolio-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.execution_role.arn
+
+  container_definitions = jsonencode([{
+    name      = "portfolio-container"
+    image     = "${aws_ecr_repository.repo.repository_url}:latest"
+    essential = true
+    portMappings = [{ containerPort = 80, hostPort = 80 }]
+  }])
+}
+
+# 6. ECS Service
+resource "aws_ecs_service" "service" {
+  name            = "portfolio-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  launch_type     = "Fargate"
+  desired_count   = 1
+  
+ 
+  force_new_deployment = true 
+
+  network_configuration {
+    subnets          = data.aws_subnets.default.ids
+    security_groups  = [aws_security_group.lb_sg.id]
+    assign_public_ip = true
+  }
+}
+
+output "ecr_url" { value = aws_ecr_repository.repo.repository_url }
